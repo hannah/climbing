5cde92c8117f59cb349ede73e0e18d9027e62351
Hannah Zabarsky <h.zabarsky@gmail.com>
2014-09-11 22:40:14 -0700

    one failing feature test

--- spec/features/view_recent_postings_spec.rb
+++ spec/features/view_recent_postings_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 feature 'creating climbing location' do
   scenario 'I can create location' do
-
+    visit new_climbsite_url
+    expect(page).to have_content("Submit")
   end
 end
