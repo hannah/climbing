a18c424a87f12fd7e60b911aa7bd6d6786f1b0ee
Hannah Zabarsky <h.zabarsky@gmail.com>
2014-09-12 22:54:51 -0700

    modified feature spec and removed launchy

--- spec/features/view_recent_postings_spec.rb
+++ spec/features/view_recent_postings_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 feature 'creating climbing location' do
   scenario 'I can create location' do
     visit new_climbsite_url
-    save_and_open_page
     expect(page).to have_content("Add a new climbing site!")
+    click_on("Create Climbsite")
+    expect(page).to have_content("Please fill in all the information")
   end
 end
