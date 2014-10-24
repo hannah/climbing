c2930f85df902dd3d6568d5b59f6197350c57e81
Hannah Zabarsky <h.zabarsky@gmail.com>
2014-10-23 23:55:23 -0400

    added show and create routes to controller, show page for climbing sites views

--- app/controllers/climbsites_controller.rb
+++ app/controllers/climbsites_controller.rb
@@ -2,4 +2,24 @@ class ClimbsitesController < ApplicationController
   def new
     @climbsite = Climbsite.new
   end
+
+  def create
+    @climbsite = Climbsite.new(climbsite_params)
+    if @climbsite.save
+      redirect_to @climbsite, notice: "Sucessfully created Climbing Site"
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @climbsite = Climbsite.find(params[:id])
+  end
+
+  private
+
+
+  def climbsite_params
+    params.require(:climbsite).permit(:name, :location, :grades, :description, :gym)
+  end
 end
